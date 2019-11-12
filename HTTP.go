fcc0e1a5cfb46cdcff5774085d0c372a7fc9dd79
Jojii <jojii@jojii.de>
2019-11-12 15:10:16 +0100

    reuse httpclient

--- HTTP.go
+++ HTTP.go
@@ -9,14 +9,20 @@ import (
 	"strings"
 )
 
+var tr *http.Transport
+
+var client = &http.Client{Transport: tr}
+
 func request(url, file string, data []byte, ignoreCert bool) (string, error) {
-	tr := &http.Transport{
-		TLSClientConfig:     &tls.Config{InsecureSkipVerify: ignoreCert},
-		MaxIdleConns:        3,
-		MaxConnsPerHost:     4,
-		MaxIdleConnsPerHost: 3,
+	if tr == nil {
+		tr = &http.Transport{
+			TLSClientConfig:     &tls.Config{InsecureSkipVerify: ignoreCert},
+			MaxIdleConns:        3,
+			MaxConnsPerHost:     4,
+			MaxIdleConnsPerHost: 3,
+		}
 	}
-	client := &http.Client{Transport: tr}
+
 	var addFile string
 	if strings.HasSuffix(url, "/") {
 		if strings.HasPrefix(file, "/") {
